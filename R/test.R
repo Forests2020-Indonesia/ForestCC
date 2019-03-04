34d78871eaf2b92a1efafd9821d82388fdeb871b
bangwim <bangwim@gmail.com>
2019-03-04 16:28:21 +0700

    update

---
+++ R/test.R
@@ -0,0 +1,33 @@
+library(lidR)
+# library(mapview)
+
+rm(list=ls())
+
+las <- 
+ctg <- catalog("PROCESSED_DATA/CTG_SINGLE/")
+
+# setting the process paramaters ----
+
+opt_chunk_buffer(ctg) <- 50
+opt_chunk_size(ctg)   <- 0
+opt_cores(ctg) <- 1
+opt_filter(ctg) <- "-drop_z_below 0"
+opt_output_files(ctg) <- paste0("PROCESSED_DATA/CTG_OUT/{ORIGINALFILENAME}")
+
+system.time({
+dtm <- grid_terrain(ctg, res=0.25, algorithm = kriging())
+})
+
+opt_output_files(ctg) <- paste0("PROCESSED_DATA/SEL_NORM/{ORIGINALFILENAME}")
+ctgnorm <- lasnormalize(ctg, dtm)
+
+
+
+
+
+newctg = catalog_apply(ctg, normalize)
+fulctg <- catalog(unlist(newctg))
+fullas <- readLAS(fulctg)
+
+writeLAS(fullas, "PROCESSED_DATA/NORMALIZED_FULL/rmu_normalized.laz")
+rm(fullas)
